8ab255070b687d580e7e9cc57d6f71a85c6cc97d
Caique Lima Gonçalves <limacaiquelg@gmail.com>
2020-02-10 03:29:34 -0300

    Changes in model classes to allow access to some attributes.

--- models/Content.py
+++ models/Content.py
@@ -12,12 +12,6 @@ class Content:
         self._number_of_sentences = 7
         self._downloaded_images = []
 
-    def add_sentence(self, new_sentence: Sentence):
-        self._sentences.append(new_sentence)
-
-    def add_downloaded_image(self, new_downloaded_image: str):
-        self._downloaded_images.append(new_downloaded_image)
-
     @property
     def sentences(self):
         return self._sentences
@@ -25,3 +19,19 @@ class Content:
     @property
     def number_of_sentences(self):
         return self._number_of_sentences
+
+    @property
+    def downloaded_images(self):
+        return self._downloaded_images
+
+    def add_sentence(self, new_sentence: Sentence):
+        self._sentences.append(new_sentence)
+
+    def add_downloaded_image(self, new_downloaded_image: str):
+        self._downloaded_images.append(new_downloaded_image)
+
+    def reset_sentences_list(self):
+        self._sentences.clear()
+
+    def reset_downloaded_images_list(self):
+        self._downloaded_images.clear()
--- models/Sentence.py
+++ models/Sentence.py
@@ -3,9 +3,24 @@ class Sentence:
         self.text = text
         self._keywords = []
         self._images = []
+        self.google_search_query = None
+
+    @property
+    def keywords(self):
+        return self._keywords
+
+    @property
+    def images(self):
+        return self._images
 
     def add_keyword(self, new_keyword: str):
         self._keywords.append(new_keyword)
 
     def add_image(self, new_image: str):
         self._images.append(new_image)
+
+    def reset_keywords_list(self):
+        self._keywords.clear()
+
+    def reset_images_list(self):
+        self._images.clear()
