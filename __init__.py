2d366e8687546d9feb7e07903b4626171be87921
Caique Lima Gonçalves <limacaiquelg@gmail.com>
2020-02-10 03:49:44 -0300

    Undo comments in init file.

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from robots import input, text, image, state
 
 print('>>>>> Welcome to video-pymaker! <<<<<')
 
-# state.delete_content_directory()
-# input.robot()
-# text.robot()
+state.delete_content_directory()
+input.robot()
+text.robot()
 image.robot()
 
 print('\n>>>>> Thank you for using video-pymaker! <<<<<')
